b414d927072641e77e0389f7fece27c6ff10f839
zzzzzooooo0000099999 <155738068+zzzzzooooo0000099999@users.noreply.github.com>
2024-03-24 13:11:01 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int a;
+  int b = 0;
+
+  scanf("%d", &a);
+
+  for(int i = 1; i <= a; i++) {
+    b = b + i;
+  }
+  printf("%d", b);
+  
+  return 0;
+}
