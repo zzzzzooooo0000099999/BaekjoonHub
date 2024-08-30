397d04ee409a8104b73fd51ac436fb1a405ee720
zzzzzooooo0000099999 <155738068+zzzzzooooo0000099999@users.noreply.github.com>
2024-08-30 11:35:34 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int d = abs(b-c);
+    
+    printf("%d", a-d);
+
+    return 0;
+}
