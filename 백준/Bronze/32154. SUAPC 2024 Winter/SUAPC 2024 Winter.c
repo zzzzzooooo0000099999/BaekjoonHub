f3ea6b918438148676ad9d75ac57e91c2eaea606
zzzzzooooo0000099999 <155738068+zzzzzooooo0000099999@users.noreply.github.com>
2024-09-27 16:43:12 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int a;
+  scanf("%d", &a);
+
+  if (a == 1){
+    printf("11\n");
+    printf("A B C D E F G H J L M");
+  }
+  else if (a == 2){
+    printf("9\n");
+    printf("A C E F G H I L M");
+  }
+  else if (a == 3){
+    printf("9\n");
+    printf("A C E F G H I L M");
+  }
+  else if (a == 4){
+    printf("9\n");
+    printf("A B C E F G H L M");
+  }
+  else if (a == 5){
+    printf("8\n");
+    printf("A C E F G H L M");
+  }
+  else if (a == 6){
+    printf("8\n");
+    printf("A C E F G H L M");
+  }
+  else if (a == 7){
+    printf("8\n");
+    printf("A C E F G H L M");
+  }
+  else if (a == 8){
+    printf("8\n");
+    printf("A C E F G H L M");
+  }
+  else if (a == 9){
+    printf("8\n");
+    printf("A C E F G H L M");
+  }
+  else if (a == 10){
+    printf("8\n");
+    printf("A B C F G H L M");
+  }
+
+  return 0;
+}
